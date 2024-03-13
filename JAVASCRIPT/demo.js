25b521a674a6d4a327a353007b5090fa32eade2a
vaibhao <vaibhaokamble@gmail.com>
2024-03-13 11:21:59 +0530

    all code of anishree technologies

---
+++ JAVASCRIPT/demo.js
@@ -0,0 +1,6 @@
+
+var v1 = 100;
+var v2 = 300;
+var v = v1 + v2;
+document.write(" Addition is V :"+v);
+
---
+++ JAVASCRIPT/onmouseover.js
@@ -0,0 +1,9 @@
+function show()
+{
+    document.getElementById("b").style.background="red";
+}
+
+function display()
+{
+    document.getElementById("b").style.background="white";
+}
